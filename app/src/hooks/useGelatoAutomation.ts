bef0bd73c0385c26ba3a29eed6b4bbc8dfc46033
pyncz <pavelyanycz@gmail.com>
2023-04-13 03:34:07 +0400

    fix: Approve spending of source token before creating of the automation task

--- app/src/hooks/useGelatoAutomation.ts
+++ app/src/hooks/useGelatoAutomation.ts
@@ -1,6 +1,7 @@
 import { AutomateSDK } from '@gelatonetwork/automate-sdk'
 import type { Signer } from 'ethers'
 import { Contract } from 'ethers'
+import { useMemo } from 'react'
 import type { AutomationForm, HexAddress } from '../models'
 import { NATIVE_TOKEN_PLACEHOLDER } from '../consts'
 
@@ -10,56 +11,73 @@ export const useGelatoAutomation = (
   automationContractAddress: HexAddress,
   resolverContractAddress: HexAddress,
 ) => {
+  const automate = useMemo(() => chainId && signer
+    ? new AutomateSDK(chainId, signer)
+    : undefined,
+  [chainId, signer])
+
   const createTask = async (payload: AutomationForm) => {
-    if (signer && chainId) {
-      const automate = new AutomateSDK(chainId, signer)
+    if (signer && automate) {
+      const sourceToken = new Contract(
+        payload.sourceTokenAddress,
+        'approve(address spender, uint256 amount)',
+        signer,
+      )
 
-      const refuelContract = new Contract(
+      // Approve spending of all the tokens in the world to be able to transfer during automation
+      // TODO: Add interfaces for user/contract allowances' management
+      const approved = await sourceToken.approve(
         automationContractAddress,
-        (await import('../../../contracts/artifacts/contracts/Refuel.sol/Refuel.json')).abi,
-        signer,
+        await sourceToken.totalSupply(),
       )
 
-      const refuelResolverContract = new Contract(
-        resolverContractAddress,
-        (await import('../../../contracts/artifacts/contracts/RefuelResolver.sol/RefuelResolver.json')).abi,
-        signer,
-      )
+      if (approved) {
+        const refuelContract = new Contract(
+          automationContractAddress,
+          (await import('../../../contracts/artifacts/contracts/Refuel.sol/Refuel.json')).abi,
+          signer,
+        )
 
-      const { taskId, tx } = await automate.createTask({
-        name: payload.name,
+        const refuelResolverContract = new Contract(
+          resolverContractAddress,
+          (await import('../../../contracts/artifacts/contracts/RefuelResolver.sol/RefuelResolver.json')).abi,
+          signer,
+        )
 
-        /* Automated contract */
-        execAddress: refuelContract.address,
-        execSelector: refuelContract.interface.getSighash('execute(address, address, uint256, address, uint256)'),
-        execAbi: refuelContract.interface.format('json') as string,
-        // execData - populated from the resolver returns
+        const { taskId, tx } = await automate.createTask({
+          name: payload.name,
 
-        /* Resolver */
-        resolverAddress: refuelResolverContract.address,
-        resolverData: refuelResolverContract.interface.encodeFunctionData('checker', [
-          await signer.getAddress(), // address _recipient,
-          payload.sourceTokenAddress, // address _sourceToken,
-          0, // uint256 _sourceMaxAmount,
-          payload.watchedTokenAddress ?? NATIVE_TOKEN_PLACEHOLDER, // address _targetToken,
-          payload.replenishmentAmount, // uint256 _targetAmount,
-          payload.threshold, // uint256 _threshold
-        ]),
-        resolverAbi: refuelResolverContract.interface.format('json') as string,
+          /* Automated contract */
+          execAddress: refuelContract.address,
+          execSelector: refuelContract.interface.getSighash('execute(address, address, uint256, address, uint256)'),
+          execAbi: refuelContract.interface.format('json') as string,
+          // execData - populated from the resolver returns
 
-        startTime: 0, // immediately
-        interval: 0, // every block?
-        useTreasury: false,
-        dedicatedMsgSender: true,
-        singleExec: false,
-      })
+          /* Resolver */
+          resolverAddress: refuelResolverContract.address,
+          resolverData: refuelResolverContract.interface.encodeFunctionData('checker', [
+            await signer.getAddress(), // address _recipient,
+            payload.sourceTokenAddress, // address _sourceToken,
+            0, // uint256 _sourceMaxAmount,
+            payload.watchedTokenAddress ?? NATIVE_TOKEN_PLACEHOLDER, // address _targetToken,
+            payload.replenishmentAmount, // uint256 _targetAmount,
+            payload.threshold, // uint256 _threshold
+          ]),
+          resolverAbi: refuelResolverContract.interface.format('json') as string,
 
-      // eslint-disable-next-line no-console
-      console.log(`Task with ID ${taskId} has been created!`)
+          startTime: 0, // immediately
+          interval: 0, // every block?
+          useTreasury: false,
+          dedicatedMsgSender: true,
+          singleExec: false,
+        })
 
-      return { taskId, tx }
+        return { taskId, tx }
+      }
+
+      throw new Error('Approval failed!')
     }
   }
 
-  return { createTask }
+  return { automate, createTask }
 }
--- contracts/scripts/dev/setupUsdtBalance.ts
+++ contracts/scripts/dev/setupUsdtBalance.ts
@@ -3,7 +3,7 @@ import { ethers } from 'hardhat'
 
 // Address of the Tether ERC20 token on the mainnet (and on the dev hardhat form, apparently)
 const USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7'
-const USDT_ABI = [
+const erc20Abi = [
   'function transfer(address to, uint amount)',
   'function issue(uint amount)',
 ]
@@ -14,7 +14,7 @@ const main = async () => {
   const newOwner = devAccounts[0]
   const newOwnerAddress = await newOwner.getAddress()
   const amountPerAccount = 1000
-  const usdt = new Contract(USDT_ADDRESS, USDT_ABI, newOwner)
+  const usdt = new Contract(USDT_ADDRESS, erc20Abi, newOwner)
 
   // update owner
   const value = ethers.utils.hexZeroPad(newOwnerAddress, 32)
